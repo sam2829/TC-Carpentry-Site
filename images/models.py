aeb291dd0b1cf4d57bf6a3cf68431df544b823f1
sam2829 <samscarisbrick@outlook.com>
2024-06-16 14:40:52 +0100

    Add correct validatation for images in models.py file

--- images/models.py
+++ images/models.py
@@ -2,6 +2,32 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError  # Import ValidationError
 from PIL import Image
+from django.core.files.images import get_image_dimensions
+import os
+
+
+def validate_image(file):
+        # Ensure the file is an image
+        valid_extensions = ['jpg', 'jpeg', 'png', 'gif']
+        extension = file.name.split('.')[-1].lower()
+        if extension not in valid_extensions:
+            raise ValidationError(f"Unsupported file extension. Allowed extensions are {valid_extensions}")
+        
+        
+
+        # Optionally, check the image dimensions
+        try:
+            # Check the file size (10MB maximum)
+            filesize = file.size
+            max_filesize = 10 * 1024 * 1024  # 10MB in bytes
+            if filesize > max_filesize:
+                raise ValidationError("The maximum file size that can be uploaded is 10MB.")
+            width, height = get_image_dimensions(file)
+            max_dimension = 4096
+            if width > max_dimension or height > max_dimension:
+                raise ValidationError(f"Image dimensions are too large. Maximum width and height allowed is {max_dimension}px.")
+        except AttributeError:
+            raise ValidationError("Invalid image file.")
 
 
 class Images(models.Model):
@@ -25,7 +51,7 @@ class Images(models.Model):
         (SKIRTINGS, 'Skirtings'),
         (OTHER, 'Other'),
     ]
-    image = models.ImageField(upload_to='images/', blank=False)
+    image = models.ImageField(upload_to='images/', blank=False, validators=[validate_image])
     description = models.TextField(max_length=50, blank=False)
     work_type = models.CharField(max_length=15, choices=WORK_TYPE_CHOICES, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -44,18 +70,5 @@ class Images(models.Model):
             raise ValidationError('At least one of main page or portfolio page must be selected.')
         
         
-
-        # # Check image dimensions and size
-        print(f"Attempting to clean image: {self.image.name}")
-        img = Image.open(self.image)
-        print(f"Image dimensions: {img.width}x{img.height}, size: {self.image.size}")
-        if img.width > 4096 or img.height > 4096:
-            raise ValidationError('Image dimensions cannot exceed 4096px.')
-        if self.image.size > 2 * 1024 * 1024:
-            raise ValidationError('Image size cannot exceed 2MB.')
-        print("Validation passed successfully.")
-
-
-    def save(self, *args, **kwargs):
-        self.clean()
-        super().save(*args, **kwargs)
+    
+        
