34a24fcbee012968f3e692765f0ff22bbebc8f26
sam2829 <samscarisbrick@outlook.com>
2024-06-23 16:12:16 +0100

    Add react bootstrap and create basic navabar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar collapseOnSelect expand="lg" className="Navbar" fixed='top' >
+      <Container>
+        <Navbar.Brand href="#home" className='px-5'>TC Carpentry</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto nav-links">
+            <Nav.Link href="#features">Home</Nav.Link>
+            <Nav.Link href="#pricing">Services</Nav.Link>
+            <Nav.Link href="#pricing">Portfolio</Nav.Link>
+            <Nav.Link href="#pricing">About Us</Nav.Link>
+            <Nav.Link href="#pricing">Contact Us</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+      
+    </>
   );
 }
 
