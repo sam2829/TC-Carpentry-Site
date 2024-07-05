24283c744d53db2bf57f0e5541ba2d6f678a5713
sam2829 <samscarisbrick@outlook.com>
2024-07-05 08:50:08 +0100

    Add google fonts and font awesome links to index.html. Create new NavBar component for Navbar. Upload image for main section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,16 @@
-import './App.css';
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import Container from 'react-bootstrap/Container'
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import image from "./images/mock-photo.jpg";
 
 function App() {
   return (
     <>
-      <Navbar collapseOnSelect expand="lg" className="Navbar" fixed='top' >
-      <Container>
-        <Navbar.Brand href="#home" className='px-5'>TC Carpentry</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ms-auto nav-links">
-            <Nav.Link href="#features">Home</Nav.Link>
-            <Nav.Link href="#pricing">Services</Nav.Link>
-            <Nav.Link href="#pricing">Portfolio</Nav.Link>
-            <Nav.Link href="#pricing">About Us</Nav.Link>
-            <Nav.Link href="#pricing">Contact Us</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-      
+      <div className={styles.App}>
+        <NavBar />
+        <main>
+          <img src={image} alt="work example" />
+        </main>
+      </div>
     </>
   );
 }
---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import styles from "../styles/NavBar.module.css";
+
+const NavBar = () => {
+  return (
+    <>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        className={styles.Navbar}
+        fixed="top"
+      >
+        <Container>
+          <Navbar.Brand href="#home" className={`px-5 ${styles.Logo}`}>
+            <i class="fa-solid fa-hammer pe-3"></i>
+            TC Carpentry
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className={`ms-auto ${styles.navLinks}`}>
+              <Nav.Link href="#features">Home</Nav.Link>
+              <Nav.Link href="#pricing">Services</Nav.Link>
+              <Nav.Link href="#pricing">Portfolio</Nav.Link>
+              <Nav.Link href="#pricing">About Us</Nav.Link>
+              <Nav.Link href="#pricing">Contact Us</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default NavBar;
