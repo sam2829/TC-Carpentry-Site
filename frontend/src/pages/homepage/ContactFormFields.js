af26ac9905d2728df03907866573768a75f581a1
sam2829 <samscarisbrick@outlook.com>
2024-07-22 12:55:31 +0100

    Add send button style to form. Add state to handle form input fields handleChange

--- frontend/src/pages/homepage/ContactFormFields.js
+++ frontend/src/pages/homepage/ContactFormFields.js
@@ -2,12 +2,17 @@ import React from "react";
 import Form from "react-bootstrap/Form";
 
 // component to render input field for home page contact form
-const ContactFormFields = ({ title, type, name }) => {
+const ContactFormFields = ({ title, type, name, value, handleChange }) => {
   return (
     <>
       <Form.Group>
         <Form.Label className="my-3">{title}</Form.Label>
-        <Form.Control type={type} name={name} />
+        <Form.Control
+          type={type}
+          name={name}
+          value={value}
+          onChange={handleChange}
+        />
       </Form.Group>
     </>
   );
--- frontend/src/pages/homepage/HomepageContactForm.js
+++ frontend/src/pages/homepage/HomepageContactForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -6,9 +6,28 @@ import Form from "react-bootstrap/Form";
 
 import styles from "../../styles/HomepageContactForm.module.css";
 import ContactFormFields from "./ContactFormFields";
+import CustomButton from "../../components/CustomButton";
 
 // component to render contact form on home page
 const HomepageContactForm = () => {
+  // State for form input fields
+  const [formEmailData, setFormEmailData] = useState({
+    name: "",
+    email: "",
+    telephone: "",
+    subject: "",
+    message: "",
+  });
+  const { name, email, telephone, subject, message } = formEmailData;
+
+  // handle change in form fields
+  const handleChange = (event) => {
+    setFormEmailData({
+      ...formEmailData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
   return (
     <>
       {/** Container for form */}
@@ -20,13 +39,21 @@ const HomepageContactForm = () => {
           <h3 className="my-3">Get In Touch</h3>
           <Row>
             <Col xs={12} lg={6}>
-              <ContactFormFields title="Name" type="text" name="name" />
+              <ContactFormFields
+                title="Name"
+                type="text"
+                name="name"
+                value={name}
+                handleChange={handleChange}
+              />
             </Col>
             <Col xs={12} lg={6}>
               <ContactFormFields
                 title="Email Address"
                 type="email"
                 name="email"
+                value={email}
+                handleChange={handleChange}
               />
             </Col>
           </Row>
@@ -35,20 +62,39 @@ const HomepageContactForm = () => {
               <ContactFormFields
                 title="Telephone Number"
                 type="tel"
-                name="telephoneNumber"
+                name="telephone"
+                value={telephone}
+                handleChange={handleChange}
               />
             </Col>
             <Col xs={12} lg={6}>
-              <ContactFormFields title="Subject" type="text" name="subject" />
+              <ContactFormFields
+                title="Subject"
+                type="text"
+                name="subject"
+                value={subject}
+                handleChange={handleChange}
+              />
             </Col>
           </Row>
           <Row>
             <Col xs={12}>
               <Form.Group>
                 <Form.Label className="my-3">Message</Form.Label>
-                <Form.Control as="textarea" rows={5} name="message" />
+                <Form.Control
+                  as="textarea"
+                  rows={5}
+                  name="message"
+                  value={message}
+                  onChange={handleChange}
+                />
               </Form.Group>
             </Col>
+          </Row>
+          <Row>
+            <div className="mt-5">
+              <CustomButton title="Send" />
+            </div>
           </Row>
         </Form>
       </Container>
