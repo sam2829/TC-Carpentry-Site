795726ef4b1ebc607516eeeb38ff5d1e08ea28f9
sam2829 <samscarisbrick@outlook.com>
2024-07-27 13:40:15 +0100

    Add basic structure and conatct form to about us and contact us pages

--- frontend/src/pages/about/AboutPage.js
+++ frontend/src/pages/about/AboutPage.js
@@ -1,10 +1,58 @@
-import React from 'react'
-import styles from '../../styles/AboutPage.module.css'
+import React from "react";
+import styles from "../../styles/AboutPage.module.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
+import ContactForm from "../../components/ContactForm";
+
+// component to render the about page
 const AboutPage = () => {
   return (
-    <main className={styles.main}>AboutPage</main>
-  )
-}
+    <div className={styles.main}>
+      <Container className="my-5">
+        {/* about us information */}
+        <Row>
+          <h2 className="my-5">About us</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+            Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
+            aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
+            imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede
+            mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum
+            semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula,
+            porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem
+            ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra
+            nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
+            Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies
+            nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+            Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+            Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
+            aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
+            imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede
+            mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum
+            semper nisi. Aenean vulputate eleifend
+          </p>
+        </Row>
+        {/* contact form */}
+        <ContactForm />
+      </Container>
+    </div>
+  );
+};
 
-export default AboutPage
+export default AboutPage;
--- frontend/src/pages/contact/ContactPage.js
+++ frontend/src/pages/contact/ContactPage.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import styles from '../../styles/ContactPage.module.css'
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
+import ContactForm from "../../components/ContactForm";
+
+//  component to render the contact page
 const ContactPage = () => {
   return (
-    <main className={styles.main}>ContactPage</main>
+    <div className={styles.main}>
+      <Container className='my-5'>
+        <Row>
+          <h2 className='my-5'>Contact Us</h2>
+          <h4 className='mb-4'>Address:</h4>
+          <p>Address line 1</p>
+          <p>Abergavenny</p>
+          <p>Monmouthsire</p>
+          <p>NP7 9TH</p>
+          <h4 className='my-4'>Phone Number:</h4>
+          <p>07875922802</p>
+          <h4 className='my-4'>Email Address:</h4>
+          <p>tomcostin@outlook.com</p>
+
+        </Row>
+        <ContactForm />
+      </Container>
+    </div>
   )
 }
 
