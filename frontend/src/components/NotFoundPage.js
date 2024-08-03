6be2529cb725e6beaab179c6b38d831648efd378
sam2829 <samscarisbrick@outlook.com>
2024-08-03 16:25:51 +0100

    Add console logs to help with bugs

--- frontend/src/components/NotFoundPage.js
+++ frontend/src/components/NotFoundPage.js
@@ -1,6 +1,8 @@
 import React from "react";
 
+// component to render the not found page
 const NotFoundPage = () => {
+  console.log("NotFoundPage rendered");
   return (
     <div>
       <p>Sorry this page does not exist</p>
