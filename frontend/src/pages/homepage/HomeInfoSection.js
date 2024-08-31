82b34b8840363c17789c05a7458ded5ac0f97bb0
sam2829 <samscarisbrick@outlook.com>
2024-08-31 12:47:00 +0100

    Add actual text for homepage info and add styling for images in home info section

--- frontend/src/pages/homepage/HomeInfoSection.js
+++ frontend/src/pages/homepage/HomeInfoSection.js
@@ -19,17 +19,36 @@ const HomeInfoSection = () => {
           <Col xs={12} lg={8}>
             <h3 className="my-3">TC Carpentry</h3>
             <p className="my-3">
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-              commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-              penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-              Donec quam felis, ultricies nec, pellentesque eu, pretium quis,
-              sem. Nulla consequat massa quis enim. Donec pede justo, fringilla
-              vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
-              imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede
-              mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum
-              semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula,
-              porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem
-              ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
+              TC Carpentry is a skilled carpentry company based in Abergavenny,
+              Monmouthshire, UK. Here at TC Carpentry we have over 18 years of
+              experience in all aspects of carpentry.
+            </p>
+            <p className="my-3">
+              We provide carpentry services to all, with no job to big or to
+              small.
+            </p>
+            <p className="my-3">
+              We cover all aspects of Carpentry work including Kitchen fitting,
+              Roofing, Doors, Flooring, Skirting boards, Custom Storage,
+              Shelving, stairs and many other first and second fix work of
+              carpentry.
+            </p>
+            <p className="my-3">
+              We hope you find us a very professional company that can help
+              understand your plans and provide the high standards of work you
+              need. You can view all recent work in our{" "}
+              <Link to="/portfolio" className={styles.TextLink}>
+                portfolio
+              </Link>
+              .
+            </p>
+            <p className="my-3">
+              You can also visit our{" "}
+              <Link to="/services" className={styles.TextLink}>
+                services
+              </Link>{" "}
+              page to find out more about what we do and how we can help you get
+              exactly what you have been looking for.
             </p>
             {/** Link to contact page */}
             <Link to="/contact">
@@ -37,7 +56,11 @@ const HomeInfoSection = () => {
               <CustomButton title="Contact Us" />
             </Link>
           </Col>
-          <Col xs={12} lg={4}>
+          <Col
+            xs={12}
+            lg={4}
+            className="d-flex align-items-center justify-content-center"
+          >
             <img
               className={` my-3 ${styles.Image}`}
               src={imageOne}
@@ -47,27 +70,54 @@ const HomeInfoSection = () => {
         </Row>
         <Row className="my-5">
           {/** this column is only on display for large screens */}
-          <Col lg={4} className="d-none d-lg-block">
-            <img
-              className={` my-3 ${styles.Image}`}
-              src={imageTwo}
-              alt="work 2"
-            />
+          <Col
+            lg={4}
+            className="d-flex align-items-center justify-content-center"
+          >
+            <div className="d-none d-lg-block w-100">
+              <img
+                className={` my-3 ${styles.Image}`}
+                src={imageTwo}
+                alt="work 2"
+              />
+            </div>
           </Col>
           <Col xs={12} lg={8}>
             <h3 className="my-3">Why Choose Us?</h3>
             <p className="my-3">
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-              commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-              penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-              Donec quam felis, ultricies nec, pellentesque eu, pretium quis,
-              sem. Nulla consequat massa quis enim. Donec pede justo, fringilla
-              vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
-              imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede
-              mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum
-              semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula,
-              porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem
-              ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
+              Here at TC Carpentry we extremely dedicated and take great pride
+              in what we do, with our main goal to deliver customer satisfaction
+              to the highest of standards.
+            </p>
+            <p className="my-3">
+              Our carpenters a highly experienced and are qualified to the
+              highest standards with City and Guilds Level 3.
+            </p>
+            <p className="my-3">
+              We are a small company but we believe this gives a more personal
+              service for the customer, which helps deliver higher customer
+              satisfactions and overall a stronger commitment to higher
+              standards.
+            </p>
+            <p className="my-3">
+              All quoations are free with no obligation, so whatever carpentry
+              work you are looking for, please be sure to speak to us first. We
+              are confident that you will find our prices competitive and fair,
+              along with the high standards we provide.
+            </p>
+            <p className="my-3">
+              We will also make sure that when we finish any job, you find the
+              areas we've been working in as clean and tidy as when we came to
+              the job.
+            </p>
+            <p className="my-3">
+              If you would like to arrange a free quote today or if you have any
+              queries regarding any carpentry work, you can call us on
+              07875922802, go to the{" "}
+              <Link to="/contact" className={styles.TextLink}>
+                contact
+              </Link>{" "}
+              page or fill out the form below.
             </p>
             {/** Link to contact page */}
             <Link to="/contact">
