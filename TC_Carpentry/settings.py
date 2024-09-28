8a063c3635fa36dd277fe73aeaae71e246ebff7b
sam2829 <samscarisbrick@outlook.com>
2024-09-28 11:48:11 +0100

    Change code to allow for custom domain

--- TC_Carpentry/settings.py
+++ TC_Carpentry/settings.py
@@ -48,10 +48,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEBUG' in os.environ
 
-ALLOWED_HOSTS = [
-    os.environ.get('ALLOWED_HOST'),
-    'localhost', '127.0.0.1'
-]
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOST', '').split(',') + ['localhost', '127.0.0.1']
 
 
 # Application definition
